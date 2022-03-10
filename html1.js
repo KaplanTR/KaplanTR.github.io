7b0ba712ee39af0f6b7b4e888f13d4eaceff82aa
KaplanTR <99499606+KaplanTR@users.noreply.github.com>
2022-03-10 18:22:47 +0300

    wefw

---
+++ html1.js
@@ -0,0 +1,7 @@
+function musa(){
+    var a=document.getElementById("username").value
+    var b=document.getElementById("password").value
+    if(a =='musa' && b=='musaturk'){
+        window.open("musa2.html")
+    }
+}
